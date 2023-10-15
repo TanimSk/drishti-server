ddd437af565a7bf088c0f365d8428b39048791dc
tanimsk <sktanim5800@gmail.com>
2023-10-15 19:25:14 +0600

    req

--- drishti_server/main/views.py
+++ drishti_server/main/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import HttpResponse
+import requests
+
 
 def home(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(",")[0]
     else:
-        ip = request.META.get('REMOTE_ADDR')
-    return HttpResponse(ip)
+        ip = request.META.get("REMOTE_ADDR")
+
+    res = requests.get(f"http://ip-api.com/json/{ip}")
+
+    return HttpResponse(f"{ip}\n {res.text()}")
