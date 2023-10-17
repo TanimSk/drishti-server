a93d36862c768ad8cace844bda621fafed54da12
tanimsk <sktanim5800@gmail.com>
2023-10-18 01:47:23 +0600

    bb

--- drishti_server/drishti_server/urls.py
+++ drishti_server/drishti_server/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from main.views import home, pre_order
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/pre-order/", pre_order),
-    path("", home, name="home"),
+    path("api/pre-order/", pre_order, name="preorder"),
+    re_path(r"^", home, name="home"),
 ]
