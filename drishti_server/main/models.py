9407c5888588b86042181c6e9ba4cdfb25b634d4
tanimsk <sktanim5800@gmail.com>
2023-10-18 01:31:29 +0600

    bb

--- drishti_server/main/models.py
+++ drishti_server/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class PreOrder(models.Model):
+    product = models.CharField(max_length=500)
+    quantity = models.IntegerField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True)
+    phone_number = models.CharField(max_length=30)
+    email_address = models.EmailField()
+    address = models.TextField()
+    comment = models.TextField()
--- drishti_server/main/views.py
+++ drishti_server/main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import PreOrder
 import requests
 
 
@@ -11,3 +12,30 @@ def home(request):
 
     res = requests.get(f"http://ip-api.com/json/{ip}").json()
     return render(request, "main.html", {"country": res.get("country")})
+
+
+def pre_order(request):
+    if request.method == "POST":
+        product = request.POST.get("product")
+        quantity = request.POST.get("quantity")
+        fname = request.POST.get("fname")
+        lname = request.POST.get("lname")
+        phone_number = request.POST.get("mobile_number")
+        email_address = request.POST.get("email_address")
+        address = request.POST.get("address")
+        comment = request.POST.get("comment")
+
+        PreOrder.objects.create(
+            product=product,
+            quantity=quantity,
+            first_name=fname,
+            last_name=lname,
+            phone_number=phone_number,
+            email_address=email_address,
+            address=address,
+            comment=comment,
+        )
+
+        return HttpResponse("OK")
+
+    return HttpResponse("Invalid Method!")
